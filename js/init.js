36f05ea5478345c0b80454dc293791cc926cda8b
Thuany <50593898+Thurunany@users.noreply.github.com>
2020-12-22 10:38:40 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAQh7OR4CJcbnKMSRhFwdbM0J0a-fNArcE",
+    authDomain: "elf-education-d28ad.firebaseapp.com",
+    databaseURL: "https://elf-education-d28ad.firebaseio.com",
+    projectId: "elf-education-d28ad",
+    storageBucket: "elf-education-d28ad.appspot.com",
+    messagingSenderId: "613022081326",
+    appId: "1:613022081326:web:49ef0c7d2caa98e9d35abf"
+  };
+
+  firebase.initializeApp(firebaseConfig);
